8d26ae66c1233cc2b1229bf22e3eb94c83653474
ahmadmcer <ahmadmcer@gmail.com>
2024-04-04 04:26:30 +0700

    feat: first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Akatetsu Matsuri</h1>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#schedule">Schedule</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <section id="home">
+      <div className="h-screen flex flex-col items-center justify-center">
+        <img className="w-3/4 max-w-[720px] mb-16" src="./images/logo.png" alt="Akatetsu Matsuri Logo" />
+        <p className="font-bold tracking-[1em]">coming soon</p>
+      </div>
+    </section>
+  );
+}
