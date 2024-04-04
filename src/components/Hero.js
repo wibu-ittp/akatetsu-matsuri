7473d41f0d48ec691b6c5fdd8940a43708eac984
ahmadmcer <ahmadmcer@gmail.com>
2024-04-04 08:06:07 +0700

    feat: add header and video background

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
 export default function Hero() {
   return (
-    <section id="home">
-      <div className="h-screen flex flex-col items-center justify-center">
-        <img className="w-3/4 max-w-[720px] mb-16" src="./images/logo.png" alt="Akatetsu Matsuri Logo" />
-        <p className="font-bold tracking-[1em]">coming soon</p>
+    <section id="home" className="relative">
+      <video className="absolute w-screen h-svh object-cover" src="./videos/anime-expo.webm" autoPlay loop muted />
+      <div className="absolute w-screen h-svh flex flex-col justify-between bg-white/75 p-8 md:px-16">
+        <div className="flex gap-4 self-center">
+          <img className="h-8 md:h-12" src="./images/wibu-ittp.png" alt="Wibu ITTP" />
+          <img className="h-8 md:h-12" src="./images/ittp.png" alt="IT Telkom Purwokerto" />
+          <img className="h-8 md:h-12" src="./images/tel-u-pwt.png" alt="Menuju Tel-U Purwokerto" />
+        </div>
+        <div className="flex flex-col items-center">
+          <img className="w-3/4 max-w-[640px] mb-8 md:mb-16" src="./images/logo.png" alt="Akatetsu Matsuri Logo" />
+          <p className="text-xl md:text-2xl font-bold tracking-[.5rem] md:tracking-[1rem]">coming soo<span className="tracking-normal">n</span></p>
+        </div>
+        <div className="flex justify-end gap-4">
+          <a className="text-xl md:text-2xl font-bold" href="#contact">recruitment <FontAwesomeIcon icon={faAngleRight} /></a>
+        </div>
       </div>
     </section>
   );
